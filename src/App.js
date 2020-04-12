66ebfede7f36bfa75a5388efb20c2caa19ee5ca1
Eslam Genedy <eslamgenedy377@yahoo.com>
2020-04-12 06:36:22 +0200

    Creation of Assignment 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-// prepare to create a profile website for it 
+// prepare to create a profile website for it
+import UserInput from './assignment_one/UserInput';
+import UserOutput from './assignment_one/UserOutput' ; 
 
 import './App.css';
 
 class App extends Component {
+  state={
+    username:"genedy97"
+  }
+
+  usernameChangedHandler=(event)=>{
+    this.setState({username:event.target.value});
+  }
+
+
   render() {
     return (
       <div className="App">
-
+        <UserInput 
+        changed={this.usernameChangedHandler} 
+        currentName={this.state.username}
+        />
+        <UserOutput userName={this.state.username}/>
+        <UserOutput userName={this.state.username}/>
+        <UserOutput userName="Max" />
 
       </div>
     )
